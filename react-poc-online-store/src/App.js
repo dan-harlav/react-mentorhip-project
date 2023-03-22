624161d02288d4fe21710311fa6b384586895a99
Stefan Scrob <stefan.scrob2@klarna.com>
2023-03-22 17:21:48 +0200

    my first react app

--- react-poc-online-store/src/App.js
+++ react-poc-online-store/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const products = [
+    {
+      id: 1,
+      name: 'name-1'
+    },
+    {
+      id: 2,
+      name: 'name-2'
+    },
+    {
+      id: 3,
+      name: 'name-3'
+    },
+    {
+      id: 4,
+      name: 'name-4'
+    },
+    {
+      id: 5,
+      name: 'name-5'
+    },
+  ];
 
-export default App;
+
+  const SearchInput = ({placeholder, onChangeHandler}) => {
+
+    return (
+        <input className='search-input' placeholder={placeholder} onChange={onChangeHandler} />
+    );
+  }
+
+  const Products = ({products}) => {
+
+    return (
+        <div className="products-list">
+            {products && products.map((product) => {
+                const {id, name} = product;
+                return <div id={id} className="product">
+                    {name}
+                </div> 
+            })}
+        </div>
+    );
+  }
+
+
+  const App = () => {
+    const [filteredProducts, setFilteredProducts] = useState(products)
+    const onChangeHandler = (event) =>{
+        const searchTerm = event.target.value;
+        setFilteredProducts(products.filter((product) => product.name.includes(searchTerm)))
+    }
+    return (
+        <div className="app">
+            <h1 className="app-title">My First React App</h1>
+            <SearchInput placeholder='Search Products' onChangeHandler={onChangeHandler} />
+            <Products products={filteredProducts} />
+        </div>
+    );
+  }
+
+  export default App;
